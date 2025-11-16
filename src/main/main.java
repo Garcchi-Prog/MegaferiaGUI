70e15725d8fa1f80d4c18ded6d17a9d9f7f51acf
Garcchi-Prog <gonzalezan@uninorte.edu.co>
2025-11-16 05:49:09 -0500

    Main Ejecucion Implementado

--- src/main/main.java
+++ src/main/main.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package main;
+
+import com.formdev.flatlaf.FlatDarkLaf;
+import javax.swing.UIManager;
+import view.MegaferiaFrame;
 
 /**
  *
  * @author Garcchi
  */
 public class main {
-    
+
+    public static void main(String[] args) {
+        System.setProperty("flatlaf.useNativeLibrary", "false");
+        try {
+            UIManager.setLookAndFeel(new FlatDarkLaf());
+        } catch (Exception ex) {
+            System.err.println("Failed to initialize LaF");
+        }
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MegaferiaFrame().setVisible(true);
+            }
+        });
+    }
 }
--- src/view/MegaferiaFrame.java
+++ src/view/MegaferiaFrame.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
+
 package view;
 
 import com.formdev.flatlaf.FlatDarkLaf;
@@ -18,11 +15,7 @@ import java.util.ArrayList;
 import javax.swing.UIManager;
 import javax.swing.table.DefaultTableModel;
 
-/**
- *
- * @author jjlora
- * @author edangulo
- */
+
 public class MegaferiaFrame extends javax.swing.JFrame {
 
     private ArrayList<Stand> stands;
@@ -1768,21 +1761,7 @@ public class MegaferiaFrame extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        System.setProperty("flatlaf.useNativeLibrary", "false");
-        
-        try {
-            UIManager.setLookAndFeel(new FlatDarkLaf());
-        } catch (Exception ex) {
-            System.err.println("Failed to initialize LaF");
-        }
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new MegaferiaFrame().setVisible(true);
-            }
-        });
-    }
+    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
