2e6bb2edb21593916ad0f42cca92a91b8d33ce50
Garcchi-Prog <gonzalezan@uninorte.edu.co>
2025-11-22 11:50:07 -0500

    ignore

--- src/model/Audiobook.java
+++ src/model/Audiobook.java
@@ -1,15 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package model;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author edangulo
- */
+
 public class Audiobook extends Book {
     
     private int duration;
--- src/model/AuthorService.java
+++ src/model/AuthorService.java
@@ -1,15 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package model;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author Garcchi
- */
+
 public class AuthorService {
     public int calculateUniquePublisherQuantity(Author author) {
         ArrayList<Publisher> publishers = new ArrayList<>();
